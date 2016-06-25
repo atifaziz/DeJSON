8265d00260589080d293e6d4c9b0bc566754e97b
Atif Aziz <code@raboof.com>
2016-06-25 20:12:04 +0200

    DateTimeOffset from JSON string

--- src/JsonImporters.cs
+++ src/JsonImporters.cs
@@ -30,42 +30,47 @@ namespace DeJson
         {
             var importers = new Delegate[]
             {
-                new Func<JsonReader, bool>      (ImportBoolean       ),
-                new Func<JsonReader, int>       (ImportInt32         ),
-                new Func<JsonReader, long>      (ImportInt64         ),
-                new Func<JsonReader, float>     (ImportSingle        ),
-                new Func<JsonReader, double>    (ImportDouble        ),
-                new Func<JsonReader, DateTime>  (ImportDateTime      ),
-                new Func<JsonReader, string>    (ImportNullOrString  ),
-                new Func<JsonReader, bool?>     (ImportNullOrBoolean ),
-                new Func<JsonReader, int?>      (ImportNullOrInt32   ),
-                new Func<JsonReader, long?>     (ImportNullOrInt64   ),
-                new Func<JsonReader, float?>    (ImportNullOrSingle  ),
-                new Func<JsonReader, double?>   (ImportNullOrDouble  ),
-                new Func<JsonReader, DateTime?> (ImportNullOrDateTime),
-                new Func<JsonReader, JsonValue> (ImportJsonValue     ),
-                new Func<JsonReader, JsonObject>(JsonObject.Import   ),
+                new Func<JsonReader, bool>           (ImportBoolean             ),
+                new Func<JsonReader, int>            (ImportInt32               ),
+                new Func<JsonReader, long>           (ImportInt64               ),
+                new Func<JsonReader, float>          (ImportSingle              ),
+                new Func<JsonReader, double>         (ImportDouble              ),
+                new Func<JsonReader, DateTime>       (ImportDateTime            ),
+                new Func<JsonReader, DateTimeOffset> (ImportDateTimeOffset      ),
+                new Func<JsonReader, string>         (ImportNullOrString        ),
+                new Func<JsonReader, bool?>          (ImportNullOrBoolean       ),
+                new Func<JsonReader, int?>           (ImportNullOrInt32         ),
+                new Func<JsonReader, long?>          (ImportNullOrInt64         ),
+                new Func<JsonReader, float?>         (ImportNullOrSingle        ),
+                new Func<JsonReader, double?>        (ImportNullOrDouble        ),
+                new Func<JsonReader, DateTime?>      (ImportNullOrDateTime      ),
+                new Func<JsonReader, DateTimeOffset?>(ImportNullOrDateTimeOffset),
+                new Func<JsonReader, JsonValue>      (ImportJsonValue           ),
+                new Func<JsonReader, JsonObject>     (JsonObject.Import         ),
             };
 
             var importerByType = importers.ToDictionary(e => e.GetType().GetGenericArguments().Last(), e => e);
             Map = type => importerByType.GetValue(type, it => new Exception($"Don't know how to import {it.FullName} from JSON."));
         }
 
-        public static Importer<bool>       Boolean          = Importer.Create(ImportBoolean);
-        public static Importer<int>        Int32            = Importer.Create(ImportInt32);
-        public static Importer<long>       Int64            = Importer.Create(ImportInt64);
-        public static Importer<float>      Single           = Importer.Create(ImportSingle);
-        public static Importer<double>     Double           = Importer.Create(ImportDouble);
-        public static Importer<DateTime>   DateTime         = Importer.Create(ImportDateTime);
-        public static Importer<bool?>      NullableBoolean  = Importer.Create(ImportNullOrBoolean);
-        public static Importer<int?>       NullableInt32    = Importer.Create(ImportNullOrInt32);
-        public static Importer<long?>      NullableInt64    = Importer.Create(ImportNullOrInt64);
-        public static Importer<float?>     NullableSingle   = Importer.Create(ImportNullOrSingle);
-        public static Importer<double?>    NullableDouble   = Importer.Create(ImportNullOrDouble);
-        public static Importer<DateTime?>  NullableDateTime = Importer.Create(ImportNullOrDateTime);
-        public static Importer<string>     String           = Importer.Create(ImportNullOrString);
-        public static Importer<JsonValue>  JsonValue        = Importer.Create(ImportJsonValue);
-        public static Importer<JsonObject> JsonObject       = Importer.Create(DeJson.JsonObject.Import);
+        public static Importer<bool>            Boolean          = Importer.Create(ImportBoolean);
+        public static Importer<int>             Int32            = Importer.Create(ImportInt32);
+        public static Importer<long>            Int64            = Importer.Create(ImportInt64);
+        public static Importer<float>           Single           = Importer.Create(ImportSingle);
+        public static Importer<double>          Double           = Importer.Create(ImportDouble);
+        public static Importer<DateTime>        DateTime         = Importer.Create(ImportDateTime);
+        public static Importer<DateTimeOffset>  DateTimeOffset   = Importer.Create(ImportDateTimeOffset);
+        public static Importer<bool?>           NullableBoolean  = Importer.Create(ImportNullOrBoolean);
+        public static Importer<int?>            NullableInt32    = Importer.Create(ImportNullOrInt32);
+        public static Importer<long?>           NullableInt64    = Importer.Create(ImportNullOrInt64);
+        public static Importer<float?>          NullableSingle   = Importer.Create(ImportNullOrSingle);
+        public static Importer<double?>         NullableDouble   = Importer.Create(ImportNullOrDouble);
+        public static Importer<DateTime?>       NullableDateTime = Importer.Create(ImportNullOrDateTime);
+        public static Importer<DateTimeOffset?> NullableDateTimeOffset =
+                                                                   Importer.Create(ImportNullOrDateTimeOffset);
+        public static Importer<string>          String           = Importer.Create(ImportNullOrString);
+        public static Importer<JsonValue>       JsonValue        = Importer.Create(ImportJsonValue);
+        public static Importer<JsonObject>      JsonObject       = Importer.Create(DeJson.JsonObject.Import);
 
         static bool     ImportBoolean(JsonReader reader)    => reader.ReadBoolean();
         static int      ImportInt32(JsonReader reader)      => reader.ReadNumber().ToInt32();
@@ -91,15 +96,21 @@ namespace DeJson
         };
 
         static DateTime ImportDateTime(JsonReader reader) =>
-            System.DateTime.ParseExact(reader.ReadString(), DateTimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.AllowWhiteSpaces);
+            System.DateTime.ParseExact(reader.ReadString(), DateTimeFormats,
+                CultureInfo.InvariantCulture, DateTimeStyles.AllowWhiteSpaces);
 
-        static bool?     ImportNullOrBoolean(JsonReader reader)  => Import(reader, ImportBoolean , null as bool?    , v => v);
-        static int?      ImportNullOrInt32(JsonReader reader)    => Import(reader, ImportInt32   , null as int?     , v => v);
-        static long?     ImportNullOrInt64(JsonReader reader)    => Import(reader, ImportInt64   , null as long?    , v => v);
-        static float?    ImportNullOrSingle(JsonReader reader)   => Import(reader, ImportSingle  , null as float?   , v => v);
-        static double?   ImportNullOrDouble(JsonReader reader)   => Import(reader, ImportDouble  , null as double?  , v => v);
-        static DateTime? ImportNullOrDateTime(JsonReader reader) => Import(reader, ImportDateTime, null as DateTime?, v => v);
-        static string    ImportNullOrString(JsonReader reader)   => Import(reader, ImportString  , null             , v => v);
+        static DateTimeOffset ImportDateTimeOffset(JsonReader reader) =>
+            System.DateTimeOffset.Parse(reader.ReadString(),
+                CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
+
+        static bool?           ImportNullOrBoolean(JsonReader reader)        => Import(reader, ImportBoolean       , null as bool?          , v => v);
+        static int?            ImportNullOrInt32(JsonReader reader)          => Import(reader, ImportInt32         , null as int?           , v => v);
+        static long?           ImportNullOrInt64(JsonReader reader)          => Import(reader, ImportInt64         , null as long?          , v => v);
+        static float?          ImportNullOrSingle(JsonReader reader)         => Import(reader, ImportSingle        , null as float?         , v => v);
+        static double?         ImportNullOrDouble(JsonReader reader)         => Import(reader, ImportDouble        , null as double?        , v => v);
+        static DateTime?       ImportNullOrDateTime(JsonReader reader)       => Import(reader, ImportDateTime      , null as DateTime?      , v => v);
+        static DateTimeOffset? ImportNullOrDateTimeOffset(JsonReader reader) => Import(reader, ImportDateTimeOffset, null as DateTimeOffset?, v => v);
+        static string          ImportNullOrString(JsonReader reader)         => Import(reader, ImportString        , null                   , v => v);
 
         static JsonValue ImportJsonValue(JsonReader reader) =>
             new JsonValue(JsonBuffer.From(reader));
